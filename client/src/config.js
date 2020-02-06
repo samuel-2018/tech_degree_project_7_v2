d8ca81a90e41b3a195a7f71a00227fbc6cb1aaad
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2020-02-06 14:38:58 -0500

    Changed to production setup

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,7 @@
-export default { apiBaseUrl: 'http://localhost:5000/api' };
+// For connecting to local api
+// export default { apiBaseUrl: 'http://localhost:5000/api' };
+
+// For connecting to online api
+export default {
+  apiBaseUrl: "http://react-gallery-api.sam.stickle.us/api"
+};
